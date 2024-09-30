810faf340e7f8318d8659fcb6c240229d13d5209
Bart Silva <bartomsilva@gmail.com>
2024-09-29 23:28:21 -0300

    feat: transação segura - transactional

--- src/main/java/com/picpaysimplificado/picpaysimplificado/services/TransactionService.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/services/TransactionService.java
@@ -4,6 +4,7 @@ import com.picpaysimplificado.picpaysimplificado.domain.transaction.Transaction;
 import com.picpaysimplificado.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.picpaysimplificado.dtos.TransactionDTO;
 import com.picpaysimplificado.picpaysimplificado.repositories.TransactionRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @Service
@@ -20,20 +22,34 @@ public class TransactionService {
     UserService userService;
 
     @Autowired
-    TransactionRepository transactionRepository;
+    TransactionRepository repository;
 
     @Autowired
     RestTemplate restTemplate;
 
+    @Transactional
     public void createTransaction(TransactionDTO transaction) throws Exception {
         User sender = this.userService.findUserById(transaction.senderId().getId());
         User receiver = this.userService.findUserById(transaction.receiverId().getId());
-        this.userService.validTransction(sender, transaction.value());
+        this.userService.validTransaction(sender, transaction.value());
 
         boolean isAuthorized = this.authorizeTransaction(sender, transaction.value());
         if(!isAuthorized){
             throw new Exception("Transação não autorizada.");
         }
+
+        Transaction newTransaction = new Transaction();
+        newTransaction.setAmount(transaction.value());
+        newTransaction.setSender(sender);
+        newTransaction.setReceiver(receiver);
+        newTransaction.setTimestamp(LocalDateTime.now());
+
+        sender.setBalance(sender.getBalance().subtract(transaction.value()));
+        receiver.setBalance(sender.getBalance().add(transaction.value()));
+
+        this.repository.save(newTransaction);
+        this.userService.saveUser(sender);
+        this.userService.saveUser(receiver);
 
     }
 
