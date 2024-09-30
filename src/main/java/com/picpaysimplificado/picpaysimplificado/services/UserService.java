07fa12f405f2445a37e976ed9a3426ec5b162eed
Bart Silva <bartomsilva@gmail.com>
2024-09-29 23:26:24 -0300

    feat: nova funcionalidade user service

--- src/main/java/com/picpaysimplificado/picpaysimplificado/services/UserService.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/services/UserService.java
@@ -1,12 +1,14 @@
 package com.picpaysimplificado.picpaysimplificado.services;
 
-import com.picpaysimplificado.picpaysimplificado.domain.UserType;
+import com.picpaysimplificado.picpaysimplificado.domain.user.UserType;
 import com.picpaysimplificado.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.picpaysimplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -14,7 +16,7 @@ public class UserService {
     @Autowired
     UserRepository repository;
 
-    public void validTransction(User user, BigDecimal amount) throws Exception {
+    public void validTransaction(User user, BigDecimal amount) throws Exception {
 
         if(user.getUsertype() == UserType.MERCHANT){
             throw new Exception("este usuário não pode efetuar trasações.");
@@ -32,8 +34,18 @@ public class UserService {
                 .orElseThrow(()-> new Exception("usuário não localizado."));
     }
 
+    public User createUser(UserDTO data){
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
     public void saveUser(User user) {
         this.repository.save(user);
+    }
+
+    public List<User> getAllUsers(){
+        return this.repository.findAll();
     }
 
 }
